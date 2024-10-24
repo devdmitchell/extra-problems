96c95444da729e71a87c3cf2e8f73b6018897a49
devdmitchell <dmitchell.prsvr@gmail.com>
2024-10-24 16:23:26 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+function isEvenlyDivisible(num1, num2) {
+return num1 % num2 === 0;
+}
+
+function halfSquare(num) {
+  return (num * num) / 2
+}
+
+function isLong(str) {
+  return str.length >= 15;
+}
+
+function exclaim() {
+}
+
+function countWords() {
+}
+
+function containsDigit() {
+}
+
+function containsLowerCase() {
+}
+
+function containsUpperCase() {
+}
+
+function containsNonAlphanumeric() {
+}
+
+function containsSpace() {
+}
+
+function digits() {
+}
+
+function truncate() {
+}
+
+function isValidPassword() {
+}
+
+function onlyPunchy(arr) {
+}
+
+
+module.exports = {
+  isEvenlyDivisible,
+  halfSquare,
+  exclaim,
+  isLong,
+  containsDigit,
+  containsLowerCase,
+  containsUpperCase,
+  containsNonAlphanumeric,
+  containsSpace,
+  countWords,
+  digits,
+  truncate,
+  isValidPassword,
+  onlyPunchy,
+}
